0dd4f983528bb5f69d3f1ce10bd692003a5c4764
ahmedkhafaga621 <ahmedkhafaga621@icloud.com>
2024-05-04 21:37:53 +0300

    upload

---
+++ lovecalc.js
@@ -0,0 +1,10 @@
+function LoveCalcolater(){
+    var firsrName = prompt("enter the firsr name please") ; 
+    var secoundName = prompt("enter the second name please") ;
+    var calc = Math.random()
+    var loveclac = calc *100 
+    var result = Math.floor(loveclac) + 1 
+    alert(result); 
+
+}
+LoveCalcolater()
